25317eb29ab112e41c1f2094b6cfffed2f94ccda
WillieCortez <31252818+WillieCortez@users.noreply.github.com>
2017-11-01 10:23:40 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,32 +1,37 @@
 //contact us-submit
 function validateForm() 
-{
+
+   {
     var x = document.contactForm.firstname.value;
     var y = document.contactForm.lastname.value;
     var z = document.contactForm.email.value;
-    
-    
+    var r = document.contactForm.contactreason.value;
+    var s = document.contactForm.subject.value;
 
-    if      (x == "" || x == null)
+    if (x == "" || x == null)
         {
             alert("Please Enter First Name.");
             return false;
         }
-            else if (y == "" || y == null)
+    else if (y == "" || y == null)
         {
-            alert("Please Ener Last Name.");
+            alert("Please Enter Last Name.");
             return false;
         }
-     
-        {    
-            else if(z == "" || z == null)       
-            
-            alert("Please Enter Your Proper Email Address.");
-            return false;
-        
+    else if(z == "" || z == null)
+        {
+            alert("Please Enter Proper Email Address.");
+            return false; 
         }
-
-            
+    else if(r == "" || r == null)
+        {
+            alert("Please Select Reason.");
+            return false; 
+        }
+    else if(s == "" || s == null)
+        {
+            alert("Please Enter Subject.");
+            return false; 
+        }
 }
-   
 
