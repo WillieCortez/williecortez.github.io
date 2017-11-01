7615f1f1e20b3fbdd80ab8a6209e0b89fe3b4a73
WillieCortez <31252818+WillieCortez@users.noreply.github.com>
2017-10-31 22:15:43 -0500

    valid email attempt

--- javascript.js
+++ javascript.js
@@ -17,11 +17,19 @@ function validateForm()
             alert("Last Name must be filled out.");
             return false;
         }
-    else if(z == "" || z == null)
-        {
-            alert("Email must be filled out.");
-            return false; 
-        }
+    else if(z = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(inputText.value.match(mailformat))  
+{  
+document.form1.text1.focus();  
+return true;  
+}  
+else  
+{  
+alert("You have entered an invalid email address!");  
+document.form1.text1.focus();  
+return false;  
+}  
+}  
     
    
 }
